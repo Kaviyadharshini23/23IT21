711736801dac41aaf711913568aa9e125441c23f
User Name <user@example.com>
2026-06-30 12:13:41 +0530

    completed task-1

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [search, setSearch] = useState("");
 
   useEffect(() => {
-    axios.get("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJzdHVkZW50cHJvZmVzc29yQGV4YW1wbGUuY29tIiwiZXhwIjoxNzgyODAwNDczLCJpYXQiOjE3ODI3OTk1NzMsImlzcyI6IkFmZm9yZCBNZWRpY2FsIFRlY2hub2xvZ2llcyBQcml2YXRlIExpbWl0ZWQiLCJqdGkiOiJhNDk1YzFlZC0yN2FmLTQ1MjYtYmQwZC02YzE5YzVkMjZmYzYiLCJsb2NhbGUiOiJlbi1JTiIsIm5hbWUiOiJrYXZpeWEgZGhhcnNoaW5pIiwic3ViIjoiYTVkYmI0MzAtZDQ5Yy00MDQxLWI4OWItYjYwM2QwMjk5MTBjIn0sImVtYWlsIjoic3R1ZGVudHByb2Zlc3NvckBleGFtcGxlLmNvbSIsIm5hbWUiOiJrYXZpeWEgZGhhcnNoaW5pIiwicm9sbE5vIjoic3R1ZGVudHByb2Zlc3NvciIsImFjY2Vzc0NvZGUiOiJXak55Q1QiLCJjbGllbnRJRCI6ImE1ZGJiNDMwLWQ0OWMtNDA0MS1iODliLWI2MDNkMDI5OTEwYyIsImNsaWVudFNlY3JldCI6IlJSblFxTkN6VG5CTVFkdHcifQ.nOs4aGcG_XJPYgsDltJzy1p1KJuE9IxeNuxi8zJ8l58")
+    axios.get("http://4.224.186.213/evaluation-service/notifications")
       .then((response) => {
         setUsers(response.data);
       });
